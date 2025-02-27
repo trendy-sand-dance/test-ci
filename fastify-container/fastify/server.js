04c793c244d0421db39d0633a5ea811d66c70697
Joppe Boeve <jboeve@f1r3s19.codam.nl>
2025-02-27 16:16:05 +0100

    Can pass vars from docker-compose

--- fastify-container/fastify/server.js
+++ fastify-container/fastify/server.js
@@ -5,12 +5,19 @@ const fastify = Fastify({
 	logger: true
 });
 
-const { ADDRESS = 'localhost', PORT = '3000' } = process.env;
+// const { ADDRESS = 'localhost', PORT = '3000' } = process.env;
+
+const ADDRESS = process.env.ADDRESS;
+const PORT = process.env.PORT;
 
 // Registering routes from routes.js
 fastify.register(routes);
 
 fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
+	console.log(`PORT : ${PORT}`)
+	console.log(`ADDRESS : ${ADDRESS}`)
+	console.log(`env : ${process.env}`)
+
 	if (err)
 	{
 		console.error(err)
