40ed20fb01a55b0f9fafa8c109e23cdfa945f039
Joppe <jboeve009@gmail.com>
2025-02-28 16:56:55 +0100

    Docker experiments (#4)
    Co-authored-by: fvan-wij <vanwijkflip@gmail.com>
    Co-authored-by: Joppe Boeve <jboeve@f1r3s19.codam.nl>
    Co-authored-by: Joppe Boeve <jboeve@f1r2s16.codam.nl>

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+import routes from "./routes/routes.js"
+
+const fastify = Fastify({
+	logger: true
+});
+
+const ADDRESS = process.env.LISTEN_ADDRESS;
+const PORT = process.env.LISTEN_PORT;
+
+// Registering routes from routes.js
+fastify.register(routes);
+
+fastify.listen({ host: ADDRESS, port: parseInt(PORT, 10) }, (err, address) => {
+	console.log(`PORT : ${PORT}`)
+	console.log(`ADDRESS : ${ADDRESS}`)
+
+	if (err)
+	{
+		console.error(err)
+		process.exit(1)
+	}
+  console.log(`Server listening at ${address}`)
+});
